d28d8dc756f68077742691e37a6c1aa750c39aa4
abhi12-ayalur <abhinav.ayalur@gmail.com>
2022-10-25 06:42:04 +0000

    refactor

--- predictor.py
+++ predictor.py
@@ -1,11 +1,11 @@
 from typing import List, Dict
-from sieve.types import FrameSingleObject, SingleObject, BoundingBox, TemporalObject
-from sieve.predictors import TemporalProcessor
+from sieve.types import FrameSingleObject, SingleObject, BoundingBox, Temporal
+from sieve.predictors import TemporalPredictor
 
 import torch
 from transformers import AutoTokenizer, ViTFeatureExtractor, VisionEncoderDecoderModel 
 
-class Captioner(TemporalProcessor):
+class Captioner(TemporalPredictor):
     def setup(self):
         device='cuda'
         encoder_checkpoint = "nlpconnect/vit-gpt2-image-captioning"
@@ -16,8 +16,8 @@ class Captioner(TemporalProcessor):
         self.model = VisionEncoderDecoderModel.from_pretrained(model_checkpoint).to(device)
     
     def predict(self, frame: FrameSingleObject) -> List[SingleObject]:
-        frame_number = frame.temporal_object.frame_number
-        frame_data = frame.temporal_object.get_array()
+        frame_number = frame.get_temporal().frame_number
+        frame_data = frame.get_temporal().get_array()
         image = self.feature_extractor(frame_data, return_tensors="pt").pixel_values.to('cuda')
         clean_text = lambda x: x.replace('<|endoftext|>','').split('\n')[0]
         caption_ids = self.model.generate(image, max_length = 64)[0]
@@ -25,7 +25,7 @@ class Captioner(TemporalProcessor):
         
         return [SingleObject(
             cls='caption',
-            temporal_object=TemporalObject(
+            temporal=Temporal(
                 frame_number=frame_number,
                 bounding_box=BoundingBox.from_array([0, 0, frame.width, frame.height]),
                 text=caption_text,
